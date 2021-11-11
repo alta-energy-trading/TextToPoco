c819155399d2a5ddce2d692e5689048d1337697a
Henry <tindalh@gmail.com>
2021-11-11 09:14:30 +0000

    change signature of texttopoco.Run

--- TextToPoco/Services/IClientImporter.cs
+++ TextToPoco/Services/IClientImporter.cs
@@ -6,7 +6,6 @@ namespace TextToPoco
     {
         public bool Persist { get; set; }
         public DeserializeArgs DeserializeArgs { get; set; }
-        public List<string> Keys { get; set; }
-        
+        public List<string> Keys { get; set; }        
     }
 }
--- TextToPoco/Services/ITextToPoco.cs
+++ TextToPoco/Services/ITextToPoco.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TextToPoco
 {
     public interface ITextToPoco
     {
         event EventHandler<WarningEventArgs> Warning;
-        void Run<T>(IDbContext context, IClientImporter importerArg) where T : class, IDirty, new();
+        void Run<T>(IDbContext context, IClientImporter importerArg) 
+            where T : class, IDirty, new();
+        void Run<T>(IDbContext context, IClientImporter importerArg, IEnumerable<IClientCleaner<IDirty, IClean>> cleaners) 
+            where T : class, IDirty, new();
     }
 }
