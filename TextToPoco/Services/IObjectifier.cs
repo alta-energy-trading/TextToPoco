487efb0cadd4886b24c222852724645a83b71e96
Henry <tindalh@gmail.com>
2021-12-23 12:25:45 +0000

    revert revert

--- TextToPoco/Services/IObjectifier.cs
+++ TextToPoco/Services/IObjectifier.cs
@@ -9,10 +9,6 @@ namespace TextToPoco
             TextToPocoArgs args)
              where T : class, new();
 
-        IEnumerable<IEnumerable<T>> BatchDeserialize<T>(
-            TextToPocoArgs args)
-             where T : class, new();
-
         List<Exception> Exceptions { get; set; }
     }
 }
--- TextToPoco/Services/ITextToPoco.cs
+++ TextToPoco/Services/ITextToPoco.cs
@@ -8,8 +8,5 @@ namespace TextToPoco
         event EventHandler<WarningEventArgs> Warning;
         IEnumerable<T> Run<T>(IDbContext context, TextToPocoArgs args) 
             where T : class, new();
-
-        IEnumerable<IEnumerable<T>> BatchRun<T>(IDbContext context, TextToPocoArgs args)
-            where T : class, new();
     }
 }
