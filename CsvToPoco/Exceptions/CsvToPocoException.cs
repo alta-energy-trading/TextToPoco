c5f608fee81a92f5e7236d71366b86d5228274d7
hugo matos <h.matos@altatrading.com>
2022-10-06 12:01:10 +0100

    Made CsvToPocoException a subclass of ReaderException

--- CsvToPoco/Exceptions/CsvToPocoException.cs
+++ CsvToPoco/Exceptions/CsvToPocoException.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 
 namespace CsvToPoco.Exceptions
 {
-    public class CsvToPocoException : Exception
+    public class CsvToPocoException : ReaderException
     {
         public string FileName { get; set; }
         public Exception Exception { get; set; }
 
-        public CsvToPocoException(string fileName, Exception exception) : base()
+        public CsvToPocoException(CsvContext context, string fileName, Exception exception) : base(context, exception.Message)
         {
             FileName = fileName;
             Exception = exception;
